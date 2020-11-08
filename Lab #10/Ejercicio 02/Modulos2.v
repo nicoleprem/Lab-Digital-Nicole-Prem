612b83611e815ee00dd3075be1e1d177643c7d7f
pre18337 <42979660+nicoleprem@users.noreply.github.com>
2020-11-07 23:57:35 -0600

    Add files via upload

---
+++ Lab #10/Ejercicio 02/Modulos2.v
@@ -0,0 +1,62 @@
+//Nicole Prem 18337 Ejercicio 02 del lab 10
+
+module tristate_data_buss(input[4:0]in, input en, output tri [4:0]out); //bussdriver que lleva los operandos 
+assign out= en ? in : 5'bz;
+endmodule
+
+module tristate_ALU_result(input[4:0]inA, input enA, output tri [4:0]outA); //bussdriver que devuelve el valor de la ALU
+assign outA= enA ? inA : 5'bz;
+endmodule
+
+module ALU (input wire [2:0]F, input wire [4:0]A, B, output reg [4:0]Y, output C, ZE);
+always @ (*)
+begin 
+    case (F)
+    'b000: begin 
+            Y <= A; C <= Y[0];
+    end 
+    'b001: begin 
+            Y <= A - B; C <= Y[4];
+    end
+    'b010: begin 
+            Y <= B; C <= Y[0];
+    end 
+    'b011: begin 
+            Y <= A + B; C<= Y[4];
+    end 
+    'b100: begin 
+            Y <= ~(A & B); C <= Y[0];
+    end 
+    endcase
+if (Y==0)
+    ZE=1;
+else 
+    ZE=0;
+  
+end 
+endmodule 
+
+module FFD1 (input clk, reset, D, enable, output reg salidaF); //FF tipo D de 1 bit
+always @(posedge clk, posedge reset )
+begin
+    if (reset)
+    salidaF <= 'b0;
+    else if (enable && clk)
+    salidaF <= D;
+end
+endmodule 
+
+module Accu(input clk, reset, input [4:0]D2, input enable, output wire [4:0] ACCU); //FF tipo D de 5 bits
+FFD1 D0(clk, reset, D2[0], ACCU[0]);
+FFD1 D1(clk, reset, D2[1], ACCU[1]);
+FFD1 D8(clk, reset, D2[2], ACCU[2]);
+FFD1 D3(clk, reset, D2[3], ACCU[3]);
+FFD1 D4(clk, reset, D2[4], ACCU[5]);
+endmodule
+
+module Procesador(input [2:0]F, input [4:0]A, B, input clk, reset, enableDB, enableR, enableALU, enableFF, output [4:0]outDB, outR, outALU, outFF, output  C, ZE);
+tristate_data_buss TDB(A, enableDB, outDB);
+Accu AC(clk, reset, outALU, enableALU, outFF);
+ALU Al(F, outFF, outDB, outALU, C, ZE);
+tristate_ALU_result AR(outALU, enableR, outR);
+endmodule
---
+++ Lab #10/Ejercicio 02/Modulos2_tb.v
@@ -0,0 +1,35 @@
+//Nicole Prem 18337 Testbench del Ejercicio 02 del Lab 10
+
+module Testbench ();
+reg clk, reset, enableDB, enableR, enableALU, enableFF;
+reg [4:0] A,B;
+reg [2:0]F;
+wire C, ZE;
+wire [4:0]outDB, outR, outALU, outFF;
+
+always begin #2 clk = ~clk; end
+
+Procesador PE(F, A, B, clk, reset, enableDB, enableR, enableALU, enableFF, outDB, outR, outALU, outFF, C, ZE);
+initial begin
+$display ("\n");
+$display ("Ejercicio 02 de Lab 10");
+$display ("F A B clk reset enableDB enableR enableALU enableFF | outDB outR outALU outFF C ZE");
+$display ("------------------------------|-------");
+$monitor ("%b %b %b %b %b %b  %b %b %b| %b %b %b  %b %b %b", F, A, B, clk, reset, enableDB, enableR, enableALU, enableFF, outDB, outR, outALU, outFF, C, ZE);
+F=3'b000; A=5'b00000; B=5'b00000; clk=0; reset=0; enableDB=0; enableR=0; enableFF=0; enableALU=0;
+#10 reset=1;
+#10 reset=0;
+#10 enableDB=1; enableR=1; enableFF=1; enableALU=1;
+#10 B=5'b00011; F=3'b010;
+#10 B=5'b00001;
+#10 F=011;
+#10 B=5'b00100;
+#10 F=001;
+#10 $finish;
+end 
+
+initial begin
+    $dumpfile ("Modulos2_tb.vcd");
+    $dumpvars (0, Testbench);
+end
+endmodule 
